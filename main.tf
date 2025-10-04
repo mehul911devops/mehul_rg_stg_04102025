81da9c06efd0f2719eca52c1fd340a1dcf82ded0
mehul911devops <mehul911@gmail.com>
2025-10-04 20:22:26 +0530

    modified stgname

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "azurerm_resource_group" "rggroup" {
 
 resource "azurerm_storage_account" "mystorageaccount" {
     depends_on = [ azurerm_resource_group.rggroup ]
-    name = "mystorage"
+    name = "mystorage041025"
     location = "eastus"
     resource_group_name = "myrg1"
     account_tier = "Standard"
